70c0041861ecd2ecde07db99a96c11984bd60a27
Exidex <16986685+Exidex@users.noreply.github.com>
2024-10-21 07:33:51 +0200

    Style and docusaurus options tweaks

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,23 +3,21 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Gauntlet',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://project-gauntlet.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'project-gauntlet',
+  projectName: 'gauntlet',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -50,10 +48,13 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: "dark"
+    },
     navbar: {
       title: 'Gauntlet',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Gauntlet Logo',
         src: 'img/logo.svg',
       },
       items: [
